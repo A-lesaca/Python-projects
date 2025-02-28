9146f7eae8bb5181a2bd592afcb51c8189d3141a
anjuloh <c4037116@newcastle.ac.uk>
2025-02-28 19:23:22 +0000

    Gym inspired

---
+++ Beginner /madlibsgame.py
@@ -0,0 +1,8 @@
+#Madlibs game
+#It's a word game where you fill in the gaps and fill in the gaps with random words
+verb1=input("Enter an appriorate verb to complete the sentence:")
+noun1=input("Enter an appriorate noun  to complete the sentence:")
+
+print(f"Today I went to went to the gym and {verb1} in my lifts")
+print(f"My favourite exercise was the{noun1}machine")
+finished=input("Well done!")
